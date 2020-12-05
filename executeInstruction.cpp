a01c3e16b8736f836f1904c879bbc06334655cbd
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2020-12-05 18:14:57 -0500

    Updated files

--- executeInstruction.cpp
+++ executeInstruction.cpp
@@ -1,36 +1,242 @@
-#define NOP_OPCODE 0x18
-#define HALT_OPCODE 0x19
-#define MATH_OPCODE 0x80
-#define MATH_FUN_OPCODE 0x70
-#define AND_OPCODE 0x00
-#define OR_OPCODE 0x10
-#define XOR_OPCODE 0x20
-#define ADD_OPCODE 0x30
-#define SUB_OPCODE 0x40
-#define INC_OPCODE 0x50
-#define DEC_OPCODE 0x60
-#define NOT_OPCODE 0x70
-#define MATH_DES_OPCODE 0x0c
-#define MATH_DES_IND_OPCODE 0x00
-#define MATH_DES_ACC_OPCODE 0x04
-#define MATH_DES_ADR_OPCODE 0x08
-#define MATH_DES_MEM_OPCODE 0x0c
-#define MATH_SRC_OPCODE 0x03
-#define MATH_SRC_IND_OPCODE 0x00
-#define MATH_SRC_ACC_OPCODE 0x01
-#define MATH_SRC_CON_OPCODE 0x02
-#define MATH_SRC_MEM_OPCODE 0x03
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 void executeInstruction(void);
 
 void executeInstruction(void) {
-    if ((IR & MATH_OPCODE) == MATH_OPCODE) {
-        switch (IR & MATH_FUN_OPCODE) {
-            case AND_OPCODE:
-                switch (IR & MATH_DES_OPCODE) {
-                    case MATH_DES_IND_OPCODE:
-                        
-                }
-        }
+
+  if ((IR & 0x80) == 0x80) { // Math OPCODE
+    int dest;
+    int source;
+
+    switch (IR & 0x0c) { // Destination
+
+      case 0: // Indirect (MAR as pointer)
+        dest = memory[MAR];
+        break;
+
+      case 4: // Accumulator ACC
+        dest = ACC;
+        break;
+
+      case 8: // Address register MAR
+        dest = MAR;
+        break;
+
+      case 12: // Memory
+        if ((IR & 0x02) == 0)
+          dest = memory[((memory[PC-2] << 8) + memory[PC-1])];
+        else
+          dest = memory[((memory[PC-4] << 8) + memory[PC-3])];
+        break;
     }
+
+    switch (IR & 0x03) { // Source
+
+      case 0: // Indirect (MAR used as a pointer)
+        source = memory[MAR];
+        break;
+
+      case 1: // Accumulator ACC
+        source = ACC;
+        break;
+
+      case 2: // Constant
+        source = memory[PC-1];
+        if ((IR & 0x0c) == 0x8)
+          source += memory[PC-2] << 8;
+        break;
+
+      case 3: // Memory
+        source = memory[((memory[PC-2] << 8) + memory[PC-1])];
+        if ((IR & 0x0c) == 0x8) {
+          source <<= 8;
+          source += memory[(memory[PC-2] << 8) + memory[PC-1]+1];
+        }
+        break;
+    }
+
+    switch (IR & 0x70){ // Checking the function
+      case 0x00: // AND
+        dest &= source;
+        break;
+      case 0x10: // OR
+        dest |= source;
+        break;
+      case 0x20: // XOR
+        dest ^= source;
+        break;
+      case 0x30: // ADD
+        dest += source;
+        break;
+      case 0x40: // SUB
+        dest -= source;
+        break;
+      case 0x50: // INC
+        dest++;
+        break;
+      case 0x60: // DEC
+        dest--;
+        break;
+      case 0x70: // NOT
+        dest = !dest;
+        break;
+    }
+
+    switch (IR & 0x0c) { // Store destination
+
+      case 0x0: // Indirect (MAR used as pointer)
+        memory[MAR] = dest & 0xff;
+        break;
+
+      case 0x4: // Accumulator ACC
+        ACC = dest & 0xff;
+        break;
+
+      case 0x8: // Address register MAR
+        MAR = dest & 0xffff;
+        break;
+
+      case 0xc: // Memory
+        memory[((memory[PC-2] << 8) + memory[PC-1])] = (dest >> 8) & 0xff;
+        memory[((memory[PC-2] << 8) + memory[PC-1]) + 1] = dest & 0xff;
+        break;
+    }
+  } // End if Math OPCODE
+
+  else if ((IR & 0xf0) == 0) { // Memory OPCODE
+
+    if ((IR & 0x08) == 0) { // Store
+
+      if ((IR & 0x04) == 0) { // Storing from Register = Accumulator ACC
+
+        switch (IR & 0x03) {
+
+          case 0: // Storing with Method = Operand is used as address
+            memory[memory[((memory[PC-2] << 8) + memory[PC-1])]] = ACC;
+            break;
+
+          case 1: // Operand is used as a Constant
+            memory[((memory[PC-2] << 8) + memory[PC-1])] = ACC;
+            break;
+
+          case 2: // Storing with Method = Indirect (MAR used as a pointer)
+            memory[MAR] = ACC;
+            break;
+        }
+      }
+
+      else { // Storing from Register = Index register MAR
+
+        switch (IR & 0x03) {
+
+          case 0: // Storing with Method = Operand is used as address
+            memory[memory[((memory[PC-2] << 8) + memory[PC-1])]] = (MAR >> 8) & 0xff;
+            memory[memory[((memory[PC-2] << 8) + memory[PC-1])] + 1] = MAR & 0xff;
+            break;
+
+          case 1: // Operand is used as a Constant
+            memory[((memory[PC-2] << 8) + memory[PC-1])] = (MAR >> 8) & 0xff; 
+            memory[((memory[PC-2] << 8) + memory[PC-1]) + 1] = MAR & 0xff;
+            break;
+
+          case 2: // Storing with Method = Indirect (MAR used as a pointer)
+            memory[MAR] = (MAR >> 8) & 0xff;
+            memory[MAR+1] = MAR & 0xff;
+          break;
+        }
+      }
+    }
+    else { // Load
+
+      if ((IR & 0x04) == 0) { // Loading into register Accumulator ACC
+
+        switch (IR & 0x03) {
+
+          case 0: // Method = Operand is used as address
+            ACC = memory[((memory[PC-2] << 8) + memory[PC-1])];
+            break;
+
+          case 1: // Method = Operand is used as a constant
+            ACC = memory[PC-1];
+            break;
+
+          case 2: // Method = Indirect (MAR used as a pointer)
+            ACC = memory[MAR];
+            break;
+        }
+      }
+
+      else { // Loading into MAR
+
+        switch (IR & 0x03) {
+
+          case 0: // Method = Operand is used as address
+            MAR = (memory[PC-2] << 8) + memory[PC-1];
+            MAR = (memory[MAR] << 8) + memory[MAR+1];
+            break;
+
+          case 1: // Method = Operand is used as a constant
+            MAR = (memory[PC-2] << 8) + memory[PC-1];
+            break;
+
+          case 2: // Method = Indirect (MAR used as pointer)
+            MAR = (memory[MAR] << 8) + memory[MAR+1];
+            break;
+        }
+      }
+    }
+  } // End if memory OPCODE
+
+  else if ((IR & 0xF8) == 0x10) { // Branches
+
+    switch (IR & 0x07) { // Type of branch
+
+      case 0: // BRA (Unconditional branch/branch always)
+        PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 1: // BRZ (Branch if ACC = 0)
+        if (ACC == 0)
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 2: // BNE (Branch if ACC != 0)
+        if (ACC != 0)
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 3: // BLT (Branch if ACC < 0)
+        if ((ACC & 0x80) != 0)
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 4: // BLE (Branch if ACC <= 0)
+        if (((ACC & 0x80) != 0) || (ACC == 0))
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 5: // BGT (Branch if ACC > 0)
+        if (((ACC & 0x80) == 0) && (ACC != 0))
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+
+      case 6: // BGE (Branch if ACC >= 0)
+        if ((ACC & 0x80) == 0)
+          PC = (memory[PC-2] << 8) + memory[PC-1];
+        break;
+    }
+  }
+
+  // All else is either a "No Operation", "Halt", or illegal opcode,
+  // in which case do nothing.
 }
--- fetchNextInstruction.cpp
+++ fetchNextInstruction.cpp
@@ -1,7 +1,94 @@
+#define NOP_OPCODE 0x18
+#define HALT_OPCODE 0x19
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 void fetchNextInstruction(void);
+void executeInstruction(void);
+
+// Declare variables
+unsigned char memory[65536];
+unsigned char ACC = 0;    // Accumulator
+unsigned char IR = 0;    // Instruction Register
+unsigned int MAR = 0;    // Memory Address Register
+unsigned int PC = 0;    // Program Counter
+unsigned int operand = 0;    // Avoid reading out of file
+
+int main(void) {
+  cout << "Hello World";
+  return 0;
+}
+
+// test function and read in file
+int test(void) {
+  string line;
+  ifstream read_memory;
+  int i = 0;
+
+  read_memory.open("mem_in.txt")
+  while (std::getline(read_memory, line)) {
+    istringstream stream_in((line));
+    for (unsigned hex_to_bin; stream_in >> hex >> hex_to_bin;)
+      memory[i++] = hex_to_bin;
+  }
+  while(PC < 65536 && (memory[PC]) != HALT_OPCODE){
+    fetchNextInstruction();
+    executeInstruction();
+  }
+
+  FILE *output
+  output = fopen("mem_out.txt", "w");
+  for (unsigned int i = 0; i < 65536;) {
+    for (int k = 0; k < 16 && i < 65536; k++)
+      fprintf(output, "%02X", memory[i++]);
+    fprintf(output, "\n");
+  }
+  fclose(output);
+  return 0;
+}
 
 void fetchNextInstruction(void) {
-    PC += 1;
-    IR = memory[PC];
-    return 0;
+  IR = memory[PC++];
+
+  if (IR & 0x80) { // Math OPCODE
+
+    if ((IR & 0x0c) == 0x0c) // Destination: memory
+      PC += 2
+
+    switch (IR & 0x03) { // Source
+
+      case 3: // Constant
+        if ((IR & 0x08) == 0x08) // 16-bit destination
+          PC++;
+        PC++;
+        break;
+
+      case 4: // Memory
+        PC += 2;
+        break:
+    }
+  } // End if math OPCODE
+
+  else if ((IR & 0xf0) == 0) { // Memory
+    if ((IR & 2) == 0) { // Operand used
+      PC++;
+      if (((IR & 1) == 0) || ((IR & 4) == 4)) // Address is operand or register is MAR
+        PC++;
+    }
+  } // End if memory OPCODE
+
+  else if ((IR & 0xF8) == 0x10) // Branches
+    PC += 2;
+
+  // All else is either a "No Operation", "Halt", or illegal opcode,
+  // in which case do nothing.
 }
